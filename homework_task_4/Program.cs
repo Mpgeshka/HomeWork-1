5c0f5495b25d0e59ce30a92c5016d5fc7fc30d1d
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-02 13:47:27 +0500

    add new task 4

---
+++ homework_task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+        count = count + 1;
+    }
+    else if (count % 2 == 1)
+    {
+        count = count + 1;
+    }
+}
