456ec1f79a1ab3d8c832d986d4fd62e564b0559d
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-08 20:43:42 +0500

    add new task_1

---
+++ Seminar_3/homewok_2_task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int Prompt (string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void CheckPalindromicNumber(int A)
+{
+    if (A > 9999 && A < 100000)
+    {
+        int a = A / 10000;
+        int b = A % 10;
+
+        if (a == b)
+        {
+            A = A / 10;
+            int c = (A / 100) % 10;
+            int d = A % 10;
+            if (c == d)
+                Console.WriteLine($"Число {A} является палиндромом");
+        }
+        else
+            Console.WriteLine($"Число {A} не является палиндромом");
+    }
+    else
+        Console.WriteLine("Некорректное число!");
+}
+
+int A = Prompt("Введите пятизначное число: ");
+CheckPalindromicNumber(A);
