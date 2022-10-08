13614a1f9f4d3f162724a937bca5dfab08df53fd
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-08 23:21:01 +0500

    add new task_1_2

--- Seminar_3/homewok_2_task_1/Program.cs
+++ Seminar_3/homewok_2_task_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Напишите программу, которая принимает на вход 
 // пятизначное число и проверяет, является ли оно палиндромом.
+
 int Prompt (string message)
 {
     Console.Write(message);
@@ -16,11 +17,16 @@ void CheckPalindromicNumber(int A)
 
         if (a == b)
         {
-            A = A / 10;
-            int c = (A / 100) % 10;
-            int d = A % 10;
+            int c = A / 1000 % 10;
+            int d = A / 10 % 10;
             if (c == d)
+            {
                 Console.WriteLine($"Число {A} является палиндромом");
+            }
+            else
+            {
+            Console.WriteLine($"Число {A} не является палиндромом");
+            }        
         }
         else
             Console.WriteLine($"Число {A} не является палиндромом");
