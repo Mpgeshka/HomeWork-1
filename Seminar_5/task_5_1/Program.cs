502c6afda57d42961c32ebc712f48aeb1bcfc227
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-16 16:51:32 +0500

    add new task_1

---
+++ Seminar_5/task_5_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// 
+
+// 
+
+int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count +1;
+        }
+
+    }
+    return count;
+}
+
+
+
+int[] arr = CreateArray(8);
+PrintArray(arr);
+int count = CountEven(arr);
+
+System.Console.WriteLine($"Количество четных чисел массива равно {count}");
