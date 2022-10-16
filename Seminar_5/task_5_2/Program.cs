1c860e7ad04c18220e350fdae70d222204c65ba1
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-16 17:08:30 +0500

    add new task_2

---
+++ Seminar_5/task_5_2/Program.cs
@@ -0,0 +1,46 @@
+﻿// 
+
+// 
+
+int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int SumOddIndexes(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+
+    }
+    return sum;
+}
+
+
+int[] arr = CreateArray(8);
+PrintArray(arr);
+int sumOddInd = SumOddIndexes(arr);
+
+System.Console.WriteLine($"Сумма чисел стоящих на нечетных позициях {sumOddInd}");
+
+
