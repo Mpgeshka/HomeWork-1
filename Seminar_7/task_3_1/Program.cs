24025b3b1fd5f974c3aabfefff7ff2a0f6178de6
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-23 22:51:00 +0500

    added new task_3_1

---
+++ Seminar_7/task_3_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+Random rnd = new Random();
+int n = rnd.Next(5, 10);
+int m = rnd.Next(5, 10);
+int[,] array = new int[n, m];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(String.Format("{0:F2}", avarage) + "; ");
+}
