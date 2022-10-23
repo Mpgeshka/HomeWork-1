d23089b9ce6d7b271e3927561faefe8b114e2ef0
Mpgeshka <mpgeshka-05@mail.ru>
2022-10-23 22:14:30 +0500

    added new task_1

---
+++ Seminar_7/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+double[,] CreateArray(int lenRows, int lenColumns)
+{
+    double[,] array = new double[lenRows, lenColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray(3, 4);
+PrintArray(array);
