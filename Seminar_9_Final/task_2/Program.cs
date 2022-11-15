ca4dce21bba5c2d94f326beacaf4a5f442bc2bcc
Mpgeshka <mpgeshka-05@mail.ru>
2022-11-15 21:08:22 +0500

    added new task2

---
+++ Seminar_9_Final/task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+int Promt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+void NumberSum (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    NumberSum(m, n, sum);
+}
+
+int n = Promt("Введите число N");
+int m = Promt("Введите число M");
+
+NumberSum(m, n, 0);
