8c0298b6279f107f6f075c06774dd667035ce8b5
Mpgeshka <mpgeshka-05@mail.ru>
2022-11-15 21:42:21 +0500

    added new task3

---
+++ Seminar_9_Final/task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Promt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+int n = Promt("Введите число N");
+int m = Promt("Введите число M");
+AkkermanFunction(m, n);
