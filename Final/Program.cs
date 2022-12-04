615e340f0b94fd1a395696deb09a07f649f31f9c
Mpgeshka <mpgeshka-05@mail.ru>
2022-12-04 18:26:47 +0500

    add first commit

--- Final/Program.cs
+++ Final/Program.cs
@@ -5,3 +5,17 @@
 // При решение не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами
 
+string[] array1 = new string[5] {"123", "^-)", "hell", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
